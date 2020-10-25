25d6199e5d0461e774390fb2f50d2993ce4c371f
Alan Tai <alan5710804@gmail.com>
2020-10-25 13:42:09 +0800

    add check file

--- p.py
+++ p.py
@@ -1,12 +1,19 @@
-# 讀取檔案
+import os # operating system
+
 products = []
-with open('products.csv', 'r', encoding='utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue
-		name, price = line.strip().split(',')
-		products.append([name, price])
-print(products)
+if os.path.isfile('products.csv'):
+	print('Yes, the file was appear.')
+	with open('products.csv', 'r', encoding='utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	print(products)
+
+else:
+	print('No, the file was disappear.')
+
 # 使用者輸入
 while True:
 	name = input('請輸入商品名稱：')
